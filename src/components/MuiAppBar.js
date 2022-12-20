76f1b4670b5dd9763a620dc9c9f32e5bb9ee5d09
Bruno Rodrigues <rodrigues.dgm@gmail.com>
2022-12-20 13:46:49 +0000

    second commit

--- src/components/MuiAppBar.js
+++ src/components/MuiAppBar.js
@@ -1,45 +1,52 @@
-import React, { useContext } from "react";
-import { AppBar, Toolbar, styled, alpha, Box, Typography, InputBase } from "@mui/material";
-import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import { SearchContext } from "../pages/Home";
+import React, { useContext } from 'react';
+import { AppBar } from '@mui/material/AppBar';
+import { Toolbar } from '@mui/material/Toolbar';
+import { Typography } from '@mui/material/Typography';
+import { InputBase } from '@mui/material/InputBase';
+import { styled } from '@mui/system/styled';
+import { alpha } from '@mui/material';
+import { Box } from '@mui/material/Box';
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import { SearchContext } from '../pages/Home';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
+  '&:hover': {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
     marginLeft: theme.spacing(1),
-    width: "auto",
+    width: 'auto',
   },
 }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
+const SearchIconWrapper = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
+  color: 'inherit',
+  '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
       },
     },
   },
@@ -52,7 +59,7 @@ export const MuiAppBar = ({ setSearch, search }) => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar className="app-bar" position="static">
         <Toolbar>
-          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}>
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
             WEBINK
           </Typography>
           <Search>
@@ -61,7 +68,7 @@ export const MuiAppBar = ({ setSearch, search }) => {
             </SearchIconWrapper>
             <StyledInputBase
               placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
+              inputProps={{ 'aria-label': 'search' }}
               onChange={(e) => {
                 searchContext.setValue(e.target.value);
               }}
